628de98bf591d7f20a4b3c04b431620d5a388bc7
veloii <85405932+veloii@users.noreply.github.com>
2024-05-31 22:28:50 +0100

    fix: site url on gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://classchartsapi.github.io",
+  base: "api-docs",
   prefetch: true,
   integrations: [
     starlight({
